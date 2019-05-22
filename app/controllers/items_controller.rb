7b5e12dab939b27db661eb6fe95628c5d21ea0e9
David Hardy <davehardy632@gmail.com>
2019-05-22 16:58:53 -0600

    created feature test for items index stats.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.active_items
+    @top_five = Item.top_five
   end
 end
--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -31,4 +31,53 @@ RSpec.describe 'as any user', Item do
         expect(page).to_not have_content("Price: #{item.price.to_f}")
     end
   end
-end
+  describe "Items index stats" do
+    before :each do
+      item_1 = create(:item)
+      item_2 = create(:item)
+      item_3 = create(:item)
+      item_4 = create(:item)
+      item_5 = create(:item)
+      item_6 = create(:item)
+      item_7 = create(:item)
+      item_8 = create(:item)
+      item_9 = create(:item)
+      item_10 = create(:item)
+
+      oi_1 = create(:order_item, item: item_1, quantity: 8, fulfilled: true)
+      oi_2 = create(:order_item, item: item_2, quantity: 1, fulfilled: true)
+      oi_3 = create(:order_item, item: item_3, quantity: 4, fulfilled: true)
+      oi_4 = create(:order_item, item: item_4, quantity: 6, fulfilled: true)
+      oi_5 = create(:order_item, item: item_5, quantity: 3, fulfilled: true)
+      oi_6 = create(:order_item, item: item_6, quantity: 5, fulfilled: true)
+      oi_7 = create(:order_item, item: item_7, quantity: 2, fulfilled: true)
+      oi_8 = create(:order_item, item: item_8, quantity: 7, fulfilled: true)
+      oi_9 = create(:order_item, item: item_9, quantity: 10, fulfilled: true)
+      oi_10 = create(:order_item, item: item_10, quantity: 9, fulfilled: true)
+
+    end
+    describe "As any kind of user, I see stats on items index" do
+        it "I see top 5 most popular items" do
+
+          visit items_path
+
+          within(".item-stats") do
+            expect(page).to have_content("Most popular items")
+            expect(page.all("p")[0]).to have_content("#{item_9.name}, bought #{oi_9.quantity} times")
+            expect(page.all("p")[1]).to have_content("#{item_10.name}, bought #{oi_10.quantity} times")
+            expect(page.all("p")[2]).to have_content("#{item_1.name}, bought #{oi_1.quantity} times")
+            expect(page.all("p")[3]).to have_content("#{item_8.name}, bought #{oi_8.quantity} times")
+            expect(page.all("p")[4]).to have_content("#{item_4.name}, bought #{oi_4.quantity} times")
+          end
+        end
+      end
+    end
+  end
+
+# As any kind of user on the system
+# When I visit the items index page ("/items")
+# I see an area with statistics:
+# - the top 5 most popular items by quantity purchased, plus the quantity bought
+# - the bottom 5 least popular items, plus the quantity bought
+#
+# "Popularity" is determined by total quantity of that item fulfilled
