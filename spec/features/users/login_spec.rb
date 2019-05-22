28de6b8ff2a13c39859f2d266683c3990934b4b6
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-05-22 13:26:34 -0600

    Adds spec for card 72

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -72,4 +72,33 @@ RSpec.describe "User log in" do
       expect(page).to have_content("Welcome #{user.name}, you are already logged in.")
     end
   end
+
+  context "When I submit invalid information" do
+    before :each do
+      @user = create(:user, role: "user")
+
+      visit login_path
+    end
+
+    it "redirects me to the login page, and I see a flash message that tells me credentials were incorrect" do
+      fill_in "email",  with: "wrong.email"
+      fill_in "password", with: @user.password
+
+      click_on "Log In"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Your credentials were incorrect.")
+      expect(page).to_not have_content("Your email was incorrect.")
+
+      fill_in "email",  with: @user.email
+      fill_in "password", with: "wrong.password"
+
+      click_on "Log In"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Your credentials were incorrect.")
+      expect(page).to_not have_content("Your password was incorrect.")
+    end
+
+  end
 end
