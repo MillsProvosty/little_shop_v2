34626f6cb359709e741e2084aea50234213b1f81
Justin D <justin@baal-software.com>
2019-05-26 08:47:12 -0600

    Adds sad path testing for merchant index spec #44

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -4,20 +4,28 @@ RSpec.describe 'Mechant index page' do
 
   context 'As an unregistered user visiting the merchants index page' do
     before :each do
-      active_merchants = create_list(:merchants, 2)
-      inactive_merchant = create(:inactive_merchant)
+      @active_merchants = create_list(:merchant, 2)
+      @inactive_merchants = create_list(:inactive_merchant, 2)
 
       visit merchants_path
     end
 
     scenario 'all active merchants in system are displayed' do
-      active_merchants.each do |merchant|
-        within "merchant-#{merchant.id}" do
+      @active_merchants.each do |merchant|
+        within "#merchant-#{merchant.id}" do
           expect(page).to have_content(merchant.name)
           expect(page).to have_content(merchant.state)
           expect(page).to have_content(merchant.city)
-          expect(page).to have_content(merchant.created_at)
+          expect(page).to have_content(merchant.date_registered)
         end
+      end
+    end
+
+    scenario 'no inactive merchants are displayed' do
+      @inactive_merchants.each do |merchant|
+        expect(page).to_not have_content(merchant.name)
+        expect(page).to_not have_content(merchant.state)
+        expect(page).to_not have_content(merchant.city)
       end
     end
   end
