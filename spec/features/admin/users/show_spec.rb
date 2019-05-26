36699b6563bb78dba6267c1b3bc273d28b610eba
David Hardy <davehardy632@gmail.com>
2019-05-26 16:41:04 -0600

    us 57 completed.

--- spec/features/admin/users/show_spec.rb
+++ spec/features/admin/users/show_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe "Users profile from admin, when I visit a users profile page" do
   before :each do
     @user = create(:user)
     @admin = create(:admin)
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
   end
   it "I see the same information the user would see, except a link to edit their profile" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
 
     visit admin_user_path(@user)
 
@@ -21,4 +20,49 @@ RSpec.describe "Users profile from admin, when I visit a users profile page" do
 
     expect(page).to_not have_link("Edit Profile")
   end
+  it "click a link upgrade user to merchant, redirect to admin_merchant_path(user), see a flash message that user has been upgraded" do
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    visit admin_user_path(@user)
+
+    click_button "Upgrade to Merchant"
+
+    expect(current_path).to eq(admin_merchant_path(@user))
+    expect(page).to have_content("#{@user.name} has been upgraded to Merchant status")
+  end
+  describe "User role is changed to merchant" do
+    it "The next time a user logs in they are now a merchant, only admin can upgrade a user to merchant" do
+
+      admin = create(:admin)
+      user_1 = create(:user)
+
+      visit login_path
+
+      fill_in "email",  with: admin.email
+      fill_in "password", with: admin.password
+      click_on "Log In"
+
+      visit admin_user_path(user_1)
+
+      click_button "Upgrade to Merchant"
+
+      click_link "Logout"
+
+      visit login_path
+
+      fill_in "email",  with: user_1.email
+      fill_in "password", with: user_1.password
+      click_on "Log In"
+
+      user_1.reload
+
+      expect(user_1.role).to eq("merchant")
+      expect(current_path).to eq(merchant_dashboard_path)
+    end
+  end
 end
+#
+# And I see a flash message indicating the user has been upgraded
+# The next time this user logs in they are now a merchant
+# Only admins can reach any route necessary to upgrade the user to merchant status
