b54a56026fd08074b5674ceb02f92a92da188baf
David Hardy <davehardy632@gmail.com>
2019-05-27 21:13:57 -0600

    Completed user story 44, also cleaned up the views and file structure a bit.

--- spec/features/merchants/items_page_spec.rb
+++ spec/features/merchants/items_page_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe "As a merchant, when I visit my items page" do
   before :each do
     @merchant = create(:merchant)
     @order_1 = create(:order)
-      @item_1 = create(:item, user: @merchant)
-      @item_2 = create(:item, user: @merchant)
-      @item_3 = create(:item, user: @merchant)
+      @item_1 = create(:item, user: @merchant, active: true)
+      @item_2 = create(:item, user: @merchant, active: false)
+      @item_3 = create(:item, user: @merchant, active: true)
         @oi_1 = create(:order_item, order: @order_1, item: @item_1)
         @oi_2 = create(:order_item, order: @order_1, item: @item_2)
         @oi_3 = create(:order_item, order: @order_1, item: @item_2)
@@ -20,14 +20,15 @@ RSpec.describe "As a merchant, when I visit my items page" do
 
       within(".links") do
       expect(page).to have_link("Add an Item")
-      end
+    end
+
       within("#item-#{@item_1.id}") do
       expect(page).to have_content(@item_1.id)
       expect(page).to have_content(@item_1.name)
       expect(page).to have_content(@item_1.image)
       expect(page).to have_content(@item_1.price)
       expect(page).to have_content(@item_1.inventory)
-      expect(page).to have_button("Edit Item #{@item_1.name}")
+      expect(page).to have_button("Edit Item")
     end
       within("#item-#{@item_2.id}") do
       expect(page).to have_content(@item_2.id)
@@ -35,7 +36,7 @@ RSpec.describe "As a merchant, when I visit my items page" do
       expect(page).to have_content(@item_2.image)
       expect(page).to have_content(@item_2.price)
       expect(page).to have_content(@item_2.inventory)
-      expect(page).to have_button("Edit Item #{@item_2.name}")
+      expect(page).to have_button("Edit Item")
     end
       within("#item-#{@item_3.id}") do
       expect(page).to have_content(@item_3.id)
@@ -43,23 +44,35 @@ RSpec.describe "As a merchant, when I visit my items page" do
       expect(page).to have_content(@item_3.image)
       expect(page).to have_content(@item_3.price)
       expect(page).to have_content(@item_3.inventory)
-      expect(page).to have_button("Edit Item #{@item_3.name}")
+      expect(page).to have_button("Edit Item")
     end
   end
   it "If no user has ever ordered the item, there is a link to delete the item" do
 
     visit merchant_items_path
 
+    within("#item-#{@item_1.id}") do
+      expect(page).to_not have_button("Delete Item")
+    end
+    within("#item-#{@item_2.id}") do
+      expect(page).to_not have_button("Delete Item")
+    end
     within("#item-#{@item_3.id}") do
-      expect(page).to have_button("Delete item")
+      expect(page).to have_button("Delete Item")
+    end
+  end
+  it "If an item is enabled, There is a button to disable the item, if disabled, button to enable" do
+
+    visit merchant_items_path
+
+    within("#item-#{@item_1.id}") do
+      expect(page).to have_button("Disable Item")
+    end
+    within("#item-#{@item_2.id}") do
+      expect(page).to have_button("Enable Item")
+    end
+    within("#item-#{@item_3.id}") do
+      expect(page).to have_button("Disable Item")
     end
   end
 end
-
-
-
-
-
-# If no user has ever ordered this item, I see a link to delete the item
-# If the item is enabled, I see a button or link to disable the item
-# If the item is disabled, I see a button or link to enable the item
