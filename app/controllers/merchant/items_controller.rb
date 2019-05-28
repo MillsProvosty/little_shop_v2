e58cfa4d233e662f8ae6f110d1e77a0c3b5e0cfb
David Hardy <davehardy632@gmail.com>
2019-05-27 21:58:02 -0600

    User story 46 complete, merchants can enable an item.

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -7,8 +7,14 @@ class Merchant::ItemsController < Merchant::BaseController
   def update
     @merchant = current_user
     @item = Item.find(params[:id])
+    if @item.active == true
     @item.update_column(:active, false)
     flash[:message] = "#{@item.name} is no longer for sale."
     redirect_to merchant_items_path
+    elsif @item.active == false
+    @item.update_column(:active, true)
+    flash[:message] = "#{@item.name} is now for sale."
+    redirect_to merchant_items_path
+    end
   end
 end
