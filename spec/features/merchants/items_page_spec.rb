5f0a307860e25eb311b9f213e88e88a171e0c206
David Hardy <davehardy632@gmail.com>
2019-05-27 20:48:55 -0600

    Test to show a delete button for an item that has never been ordered.

--- spec/features/merchants/items_page_spec.rb
+++ spec/features/merchants/items_page_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe "As a merchant, when I visit my items page" do
   before :each do
     @merchant = create(:merchant)
-
-    @item_1 = create(:item, user: @merchant)
-    @item_2 = create(:item, user: @merchant)
-    @item_3 = create(:item, user: @merchant)
+    @order_1 = create(:order)
+      @item_1 = create(:item, user: @merchant)
+      @item_2 = create(:item, user: @merchant)
+      @item_3 = create(:item, user: @merchant)
+        @oi_1 = create(:order_item, order: @order_1, item: @item_1)
+        @oi_2 = create(:order_item, order: @order_1, item: @item_2)
+        @oi_3 = create(:order_item, order: @order_1, item: @item_2)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
@@ -43,23 +46,20 @@ RSpec.describe "As a merchant, when I visit my items page" do
       expect(page).to have_button("Edit Item #{@item_3.name}")
     end
   end
+  it "If no user has ever ordered the item, there is a link to delete the item" do
+
+    visit merchant_items_path
+
+    within("#item-#{@item_3.id}") do
+      expect(page).to have_button("Delete item")
+    end
+  end
 end
 
 
 
 
 
-As a merchant
-When I visit my items page "/dashboard/items"
-I see a link to add a new item to the system
-I see each item I have already added to the system, including:
-- the ID of the item
-- the name of the item
-- a thumbnail image for that item
-- the price of that item
-- my current inventory count for that item
-- a link or button to edit the item
-
-If no user has ever ordered this item, I see a link to delete the item
-If the item is enabled, I see a button or link to disable the item
-If the item is disabled, I see a button or link to enable the item
+# If no user has ever ordered this item, I see a link to delete the item
+# If the item is enabled, I see a button or link to disable the item
+# If the item is disabled, I see a button or link to enable the item
