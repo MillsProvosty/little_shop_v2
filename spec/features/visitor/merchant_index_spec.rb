0914495c555ae7ae3c000ee9562f81e81f7a043b
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-05-29 14:32:33 -0600

    Initial commit for user story 50 feature spec

---
+++ spec/features/visitor/merchant_index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+
+RSpec.describe "As a visitor on the merchants index page" do
+  before :each do
+    
+  end
+
+  describe "I see an area with the following statistics" do
+    it "top 3 merchants who have sold the most by price and quantity, and their revenue " do
+    end
+
+    xit "top 3 merchants who were fastest at fulfilling items in an order, and their times" do
+    end
+
+    xit "worst 3 merchants who were slowest at fulfilling items in an order, and their times" do
+    end
+
+    xit "top 3 states where any orders were shipped (by number of orders), and count of orders" do
+    end
+
+    xit "top 3 cities where any orders were shipped (by number of orders, also Springfield, MI should not be grouped with Springfield, CO), and the count of orders" do
+    end
+
+    xit "top 3 biggest orders by quantity of items shipped in an order, plus their quantities" do
+    end
+  end
+end
